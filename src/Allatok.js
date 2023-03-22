c2c75b17587b87f2d83e2d108194a5a85f27a78c
natibi121 <natibi279@gmail.com>
2023-03-22 09:34:46 +0100

    1.1

---
+++ src/Allatok.js
@@ -0,0 +1,14 @@
+function Allatok() {
+    return(
+        <div id="allatok">
+            {allaok.map((allat, ix) => (
+                <Kartya key={ix} allat={allat} />
+            ))}
+        </div>
+    )
+};
+
+
+function Allat{
+    
+}
