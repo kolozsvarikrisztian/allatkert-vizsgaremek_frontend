b7d48b40911c19f467666b56f70a9ad42f4c890d
natibi121 <natibi279@gmail.com>
2023-03-22 09:43:50 +0100

    1.2

--- src/Allatok.js
+++ src/Allatok.js
@@ -1,7 +1,7 @@
 function Allatok() {
-    return(
+    return (
         <div id="allatok">
-            {allaok.map((allat, ix) => (
+            {allatok.map((allat, ix) => (
                 <Kartya key={ix} allat={allat} />
             ))}
         </div>
@@ -9,6 +9,20 @@ function Allatok() {
 };
 
 
-function Allat{
-    
+function Allat({ allat }) {
+    return (
+    <div className="card col-lg-4 kartyak">
+                <h2>Gímszarvas</h2>
+                <img  src="img-animal/gímszarvas.jpg" alt="">
+                <div>
+                    <p class="text-center">Becenév: Gombi</p>
+                    <p class="text-center">Életkor: 10 év</p>
+                    <p class="text-center">Állatfaj: Cervus elaphus</p>
+                    <p class="text-center">Származás: Európa</p>
+                    <p class="text-center">Táplálék: Lágy szárú növények, a bokrok és fák rügyei</p>
+                </div>
+            </div>
+    )
 }
+
+export default Allatok;
