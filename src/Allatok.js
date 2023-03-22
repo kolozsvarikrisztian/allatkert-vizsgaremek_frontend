5ec606806fc81e0014e586ea9b6d119a34390a36
natibi121 <natibi279@gmail.com>
2023-03-22 09:55:06 +0100

    1.3

--- src/Allatok.js
+++ src/Allatok.js
@@ -11,9 +11,9 @@ function Allatok() {
 
 function Allat({ allat }) {
     return (
-    <div className="card col-lg-4 kartyak">
-                <h2>Gímszarvas</h2>
-                <img  src="img-animal/gímszarvas.jpg" alt="">
+        <div className="">
+            <h2><allat.name></h2>
+            <img src="img-animal/gímszarvas.jpg" alt="">
                 <div>
                     <p class="text-center">Becenév: Gombi</p>
                     <p class="text-center">Életkor: 10 év</p>
@@ -21,7 +21,7 @@ function Allat({ allat }) {
                     <p class="text-center">Származás: Európa</p>
                     <p class="text-center">Táplálék: Lágy szárú növények, a bokrok és fák rügyei</p>
                 </div>
-            </div>
+        </div>
     )
 }
 
