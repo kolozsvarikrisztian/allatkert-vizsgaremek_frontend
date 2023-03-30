41d7824d0edb4f6a98ab14447d955c360f5ca2e1
natibi121 <natibi279@gmail.com>
2023-03-30 09:08:47 +0200

    1.7

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import './App.css';
 import './Home.css';
 import Home from './Home';
 import Allatok from "./Allatok";
-<<<<<<< HEAD
-=======
 import Gallery from "./Gallery";
->>>>>>> ead6908aed775656aa871fc2bd577dece319c27b
 
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       
-=======
       <Gallery />
->>>>>>> ead6908aed775656aa871fc2bd577dece319c27b
     </div>
   );
 }
